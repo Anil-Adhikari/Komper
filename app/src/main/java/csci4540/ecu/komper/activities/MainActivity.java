bf99d6ca59c41639a326a580d45f1026939b9010
Amanda <petersena06@students.ecu.edu>
2017-10-01 17:59:31 -0400

    Main layout button added for google sign-in, also added configure signin for google but is not correct(will need to fix)

--- app/src/main/java/csci4540/ecu/komper/activities/MainActivity.java
+++ app/src/main/java/csci4540/ecu/komper/activities/MainActivity.java
@@ -3,6 +3,10 @@ package csci4540.ecu.komper.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import csci4540.ecu.komper.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-    }
 
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+    }
 }
